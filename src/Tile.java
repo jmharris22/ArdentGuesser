08d55bc0f29d4297012befaeb0bce547560fd24c
Justin Harris <jharris.cds@gmail.com>
2015-06-01 21:32:06 -0700

    started working with Sprite Sheets

--- src/Tile.java
+++ src/Tile.java
@@ -1,3 +1,7 @@
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
+
 /** Represents a tile of the game world terrain
 *
 * @author Justin Harris
@@ -12,6 +16,9 @@ public class Tile {
 	    HalfSlopeWest, HalfSlopeEast, HalfSlopeNorth, HalfSlopeAdjoin, AdjoiningTriangleFace,
 	    TopHalfSlopeWest, TopHalfSlopeEast, TopHalfSlopeNorth, TopHalfSlopeSouth
 	}
+	
+	private static SpriteSheet tileSheet;
+	
 	/** Tile's vertical order in the game map */
 	private int height;
 	private Tile[] neighbors;
@@ -20,6 +27,22 @@ public class Tile {
 
 	public Tile() {
 		
+	}
+
+	public static SpriteSheet getTileSheet() {
+		if(getTileSheet() == null) {
+			try {
+				setTileSheet(new SpriteSheet(new Image("art/terrain/Tiles.png"), 64, 64));
+			} catch (SlickException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return tileSheet;
+	}
+
+	public static void setTileSheet(SpriteSheet tileSheet) {
+		Tile.tileSheet = tileSheet;
 	}
 
 	public int getHeight() {
